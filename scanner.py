e2b52e6b01f6a1d020930bd90f93603d832393af
someone25572 <someone25572@mail2tor.com>
2017-04-05 12:02:31 +0200

    first config

--- scanner.py
+++ scanner.py
@@ -1,1 +1,33 @@
-import socket
+#!/usr/bin/python
+# -*-coding:utf8;-*-
+import socket, os, threading, argparse, sys
+parser = argparse.ArgumentParser(prog='scanner', usage='%(prog)s [options]')
+parser.add_argument("-sc", 'sockconn', help='performes an sock connection')
+parser.add_argument('host', type=str, help='host to scan')
+
+args = parser.parse_args()
+
+def check_port(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    try:
+        s.connect()
+        s.close()
+        print("[+] " + host + " has " + port + " open.")
+    except:
+        pass
+
+
+
+class config(object):
+    def __init__(self):
+        pass
+
+
+try:
+    print("press a key for time")
+    while not input():
+        sys.stdout.write()
+        sys.stdout.flush()
+except KeyboardInterrupt:
+    os.popen("kill -9 " + str(os.getpid()))
